12515ddb1d5be2c124f1e15664a3fa7771d222df
Profesor <Profesor@DESKTOP-J02DD8Q>
2024-03-13 17:06:47 +0100

    Modificaion segunda

--- HelloGit/src/HelloGit.java
+++ HelloGit/src/HelloGit.java
@@ -6,6 +6,7 @@ public class HelloGit {
 		// TODO Auto-generated method stub
 		System.out.println("Hello GIT!!");
 		System.out.println("modificacion primera");
+		System.out.println("modificacion segunda");
 	}
 
 }
